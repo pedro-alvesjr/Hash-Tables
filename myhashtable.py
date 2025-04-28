78703055cb2f217ac39495bfbd1b4d342001b048
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-04-27 21:42:58 -0300

    Create myhashtable.py
    Added HashTable class, hash and print_table methods.

---
+++ myhashtable.py
@@ -0,0 +1,14 @@
+class HashTable:
+    def __init__(self, value = 7):
+        self.data_map = [None] * value
+        
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash  
+
+    def print_table(self):
+        for i, val in enumerate(self.data_map): 
+            print(i, ": ", val)
